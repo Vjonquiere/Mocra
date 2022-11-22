73c871911adf76bd8cd0402f27026af45127b824
Valentin Jonquiere <71729360+Vjonquiere@users.noreply.github.com>
2022-11-22 19:45:25 +0100

    house entity

---
+++ mocraAdventure/map/entities/house1/entity.gd
@@ -0,0 +1,22 @@
+extends StaticBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+func _on_Area2D_body_entered(body):
+	$Light2D.set_color(Color("00ff1c"))
+	$AnimationPlayer.play("open")
+
+
+func _on_Area2D_body_exited(body):
+	$Light2D.set_color(Color("ff0000"))
+	$AnimationPlayer.play("close")
